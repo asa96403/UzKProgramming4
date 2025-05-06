331cb54e7d5138fb5a8ab540736a25be58828a3e
aabert <aabert@smail.uni-koeln.de>
2025-05-06 21:14:49 +0200

    added interfaces for task 3 and prepared classses

---
+++ Homework4/src/excercise3/Lamp.java
@@ -0,0 +1,35 @@
+package excercise3;
+
+public class Lamp implements Switchable, Dimmable{
+
+	@Override
+	public void turnOn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void turnOff() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isOn() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	@Override
+	public void setBrightness(int level) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	@Override
+	public int getBrightness() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ Homework4/src/excercise3/Switchable.java
@@ -0,0 +1,7 @@
+package excercise3;
+
+public interface Switchable {
+	void turnOn();
+	void turnOff();
+	boolean isOn();
+}
