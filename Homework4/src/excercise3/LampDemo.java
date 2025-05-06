0e5ab60ee1c03c7221dcfe952e4652e3d2ed046d
aabert <aabert@smail.uni-koeln.de>
2025-05-06 23:15:09 +0200

    modified demo

--- Homework4/src/excercise3/LampDemo.java
+++ Homework4/src/excercise3/LampDemo.java
@@ -11,8 +11,10 @@ public class LampDemo {
 		System.out.println("Brightness: " + lamp.getBrightness());
 		System.out.println("On: " + lamp.isOn()+ "\n --lamp on--");
 		lamp.turnOn ();
-		System.out.println("On: " + lamp.isOn()+ "\n --set brightness--");
+		System.out.println("Brightness: " + lamp.isOn()+ "\n --set brightness 80--");
 		lamp.setBrightness (80);
+		System.out.println("On: " + lamp.getBrightness()+ "\n --set brightness 150--");
+		lamp.setBrightness (150);
 		System.out.println("Brightness: " + lamp.getBrightness()+ "\n --dim to half--");
 		lamp.dimToHalf ();
 		System.out.println (" Brightness: " + lamp.getBrightness ());
